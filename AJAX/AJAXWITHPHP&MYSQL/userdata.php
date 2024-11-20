72c3059705fa67608d3fcda2bfefd7668ddaf155
Arun Pandey Laudari <arpa_bca2079@lict.edu.np>
2024-11-20 10:37:39 +0545

    AJAX and SQL

--- AJAX/AJAXWITHPHP&MYSQL/userdata.php
+++ AJAX/AJAXWITHPHP&MYSQL/userdata.php
@@ -1,9 +1,25 @@
-
 <?php
 
-if (isset($_POST['name'])) {
-    echo "Hello, " . $_POST['name'];
-} else {
-    echo "Error";
+$search = $_POST['name'];
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "lict";
+
+$conn = new mysqli($server, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-?>
+
+$sql = "SELECT * FROM `student` WHERE `name` LIKE '%$search%'";
+$result = $conn->query($sql);
+
+$data = "";
+
+while ($row = $result->fetch_assoc()) {
+    $data .= "Name: " . $row['name'] . "<br>";
+}
+
+echo $data;
